f164e3d0da42e2a6d3853255cd9c9d0411ae9c43
Mihaela Baksic <mihaela.baksic@fer.hr>
2020-10-25 02:25:23 +0200

    User - UserDTO mapping created

--- IzvorniKod/pomozi-mi/src/main/java/hr/fer/progi/mappers/UserDTO.java
+++ IzvorniKod/pomozi-mi/src/main/java/hr/fer/progi/mappers/UserDTO.java
@@ -1,8 +1,25 @@
 package hr.fer.progi.mappers;
 
+import hr.fer.progi.domain.User;
+import hr.fer.progi.domain.UserStatus;
+
 public class UserDTO {
 
+    private String username;
+    private String name;
+    private String surname;
+    private String email;
+    //profile picture
+    private boolean administrator;
 
-
+    public User mapToUser(){
+        User user = new User();
+        user.setUsername(this.username);
+        user.setName(this.name);
+        user.setSurname(this.surname);
+        user.setEmail(this.email);
+        user.setAdministrator(this.administrator);
+        return user;
+    }
 
 }
