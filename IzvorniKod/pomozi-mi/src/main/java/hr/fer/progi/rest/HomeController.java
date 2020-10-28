4caa6c201e5e04ae914dfa8770df423871cdc301
Mihaela Baksic <mihaela.baksic@fer.hr>
2020-10-28 10:48:29 +0100

    Fixed home controller

--- IzvorniKod/pomozi-mi/src/main/java/hr/fer/progi/rest/HomeController.java
+++ IzvorniKod/pomozi-mi/src/main/java/hr/fer/progi/rest/HomeController.java
@@ -1,12 +1,25 @@
 package hr.fer.progi.rest;
 
+import hr.fer.progi.domain.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class HomeController {
 
     // ovisi o autentikaciji, bit ce napisano nakon autentikacije i autorizacija
+
+    @GetMapping("")
+    public RedirectView getUser(@AuthenticationPrincipal User user){
+        if(user== null)
+            return new RedirectView("/login");
+        else
+            return new RedirectView("/requests");
+    }
 
 }
