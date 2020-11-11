2874749d62605ea56fd98cbefc49d59fa56f87cd
Hrvoje Rom <hrvoje.rom@fer.hr>
2020-11-11 03:15:50 +0100

    testdeploy problem registracija cors

--- IzvorniKod/pomozi-mi/src/main/java/hr/fer/progi/rest/WebSecurity.java
+++ IzvorniKod/pomozi-mi/src/main/java/hr/fer/progi/rest/WebSecurity.java
@@ -51,7 +51,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Collections.singletonList("http://3.139.60.236"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
         configuration.setAllowCredentials(true);
