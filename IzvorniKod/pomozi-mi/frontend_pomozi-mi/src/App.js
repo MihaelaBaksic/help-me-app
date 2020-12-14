39b1e02df9cf6fd9cda14181a7fe9cee2640bd76
Hrvoje Rom <hrvoje.rom@fer.hr>
2020-12-14 14:30:46 +0100

    Deploy test

--- IzvorniKod/pomozi-mi/frontend_pomozi-mi/src/App.js
+++ IzvorniKod/pomozi-mi/frontend_pomozi-mi/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
 		super(props);
 
 		this.state = {
-			isLogedIn: sessionStorage.getItem("isLogedIn") || false /*true*/,
+			isLogedIn: sessionStorage.getItem("isLogedIn") || true /*true*/,
 			basicAuthToken: sessionStorage.getItem("basicAuthToken") || "",
 		};
 
