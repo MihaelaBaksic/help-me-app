94add54ec813acbf9271c32ae50cfa442900b5e8
Dominik Milde <dm51460@fer.hr>
2020-12-24 14:06:58 +0100

    dodan prikaz tudeg profila, komponenta

---
+++ IzvorniKod/pomozi-mi/frontend_pomozi-mi/src/components/OtherUserProfile.js
@@ -0,0 +1,57 @@
+function OtherUserProfile(props) {
+    return (
+        <div className="layout-content">
+            <div className="container flex-grow-1 container-p-y">
+                <div className="container-m-nx container-m-ny theme-bg-white mb-4">
+                    <div className="media col-md-10 col-lg-8 col-xl-7 py-5 mx-auto">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" className="d-block ui-w-100 rounded-circle" />
+
+                        <div className="media-body ml-5">
+                            <h4 className="font-weight-bold mb-4">Pero Perić</h4>
+
+                            <a className="d-inline-block text-body ml-3">
+                                <strong>51</strong>
+                                <span className="text-muted">izvršenih zahtjeva</span>
+                            </a>
+                            <a className="d-inline-block text-body ml-3">
+                                <strong>44</strong>
+                                <span className="text-muted">postavljenih zahtjeva</span>
+                            </a>
+                            <div className="mt-3">
+                                <a href="javascript:void(0)" className="btn btn-success rounded-pill">+&nbsp; Dodaj kao admina</a>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <hr className="m-0" />
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <div className="card mb-4">
+                            <div className="card-body">
+                                <div className="col-md-9">
+                                    OVDJE JE KOMPONENTA POSTAVLJENIH ZAHTJEVA
+                      </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="col-xl-4">
+
+                        <div className="card mb-4">
+                            <div className="card-body">
+                                OVDJE JE KOMPONENTA KOMENTARA
+                    </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default OtherUserProfile;
